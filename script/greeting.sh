02d840ce1dbe3709f26b4faeee4b6a5b05b20ada
Tamino1230 <roidingertamino@gmail.com>
2025-06-10 09:08:31 +0200

    Add user input

--- script/greeting.sh
+++ script/greeting.sh
@@ -1,3 +1,7 @@
 
-echo "Willkommen auf dem Rechner, $USER ..."
+username=$(whoami)
+echo "Willkommen auf dem Rechner, $username"
 echo "Heute ist $(date '+%Y-%m-%d %H:%M:%S')"
+
+read -p "Wie geht es dir heute? " antwort
+echo "Du hast $antwort eingegeben!"
